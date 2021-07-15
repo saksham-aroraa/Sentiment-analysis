2fda66082f47760397cd9f155d708f8c2e48599a
Saksham Arora <56152405+saksham-aroraa@users.noreply.github.com>
2021-07-16 02:14:55 +0530

    Elaboration added

--- Sentiment analysis/sentiment analysis elaborated.py
+++ Sentiment analysis/sentiment analysis elaborated.py
@@ -1,2 +1,366 @@
-import numpy
-a = numpy.array([[1,-1,2,1,0,0],[2,-3,-5,0,1,0],[-1,3,5,0,0,1]])
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  1 13:52:31 2020
+
+@author: Saksham Arora
+"""
+import re
+import pandas as pd 
+import os
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import nltk
+import warnings 
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+#######################################################
+
+#LOADING DATA
+
+#######################################################
+
+#id : The id associated with the tweets in the given dataset.
+#tweets : The tweets collected from various sources and having either positive or negative sentiments associated with it.
+#label : A tweet with label ‘0’ is of positive sentiment while a tweet with label ‘1’ is of negative sentiment.
+
+#reading the train.csv with Pandas
+try:
+    path = os.getcwd()
+except:
+    print("Try checking the current working directory.")
+
+train = pd.read_csv(path + '\\train.csv')
+#We make a copy of test data so that even if we make any changes in this dataset we would not lose the original dataset.
+train_original=train.copy()
+
+#reading the test.csv with Pandas
+test = pd.read_csv(path +'\\test.csv')
+
+#For similar reasons as for the training data, we make a copy of the original test file
+test_original=test.copy()
+
+#######################################################
+
+#DATA PRE-PROCESSING
+
+#######################################################
+
+combine = train.append(test,ignore_index=True,sort=True)
+
+#Columns not in the original dataframes are added as new columns and the new cells are populated with NaN value.
+#we can check the result by typing combine.head() to see the initial training set, and can run combine.tail() to see last lines of the code
+
+#Removing Twitter Handles(@User)
+
+def remove_pattern(text,pattern):
+    
+    # re.findall() finds the pattern i.e @user and puts it in a list for further task
+    r = re.findall(pattern,text)
+    
+    # re.sub() removes @user from the sentences in the dataset
+    for i in r:
+        text = re.sub(i,"",text)
+    
+    return text
+
+combine['Tidy_Tweets'] = np.vectorize(remove_pattern)(combine['tweet'], "@[\w]*")
+
+combine['Tidy_Tweets'] = combine['Tidy_Tweets'].str.replace("[^a-zA-Z#]", " ")
+
+combine['Tidy_Tweets'] = combine['Tidy_Tweets'].apply(lambda x: ' '.join([w for w in x.split() if len(w)>3]))
+
+tokenized_tweet = combine['Tidy_Tweets'].apply(lambda x: x.split())
+
+tokenized_tweet.head()
+
+from nltk import PorterStemmer
+
+ps = PorterStemmer()
+
+tokenized_tweet = tokenized_tweet.apply(lambda x: [ps.stem(i) for i in x])
+
+for i in range(len(tokenized_tweet)):
+    tokenized_tweet[i] = ' '.join(tokenized_tweet[i])
+
+combine['Tidy_Tweets'] = tokenized_tweet
+
+#######################################################
+
+#DATA VISUALISATION
+
+#######################################################
+
+from wordcloud import WordCloud,ImageColorGenerator
+from PIL import Image
+import urllib
+import requests
+
+all_words_positive = ' '.join(text for text in combine['Tidy_Tweets'][combine['label']==0])
+
+# combining the image with the dataset
+Mask = np.array(Image.open(requests.get('http://clipart-library.com/image_gallery2/Twitter-PNG-Image.png', stream=True).raw))
+
+# We use the ImageColorGenerator library from Wordcloud 
+# Here we take the color of the image and impose it over our wordcloud
+image_colors = ImageColorGenerator(Mask)
+
+# Now we use the WordCloud function from the wordcloud library 
+wc = WordCloud(background_color='black', height=1500, width=4000,mask=Mask).generate(all_words_positive)
+
+# Size of the image generated 
+plt.figure(figsize=(10,20))
+
+# Here we recolor the words from the dataset to the image's color
+# recolor just recolors the default colors to the image's blue color
+# interpolation is used to smooth the image generated 
+plt.imshow(wc.recolor(color_func=image_colors),interpolation="hamming")
+
+plt.axis('off')
+plt.show()
+
+all_words_negative = ' '.join(text for text in combine['Tidy_Tweets'][combine['label']==1])
+
+# combining the image with the dataset
+Mask = np.array(Image.open(requests.get('http://clipart-library.com/image_gallery2/Twitter-PNG-Image.png', stream=True).raw))
+
+# We use the ImageColorGenerator library from Wordcloud 
+# Here we take the color of the image and impose it over our wordcloud
+image_colors = ImageColorGenerator(Mask)
+
+# Now we use the WordCloud function from the wordcloud library 
+wc = WordCloud(background_color='black', height=1500, width=4000,mask=Mask).generate(all_words_negative)
+
+# Size of the image generated 
+plt.figure(figsize=(10,20))
+
+# Here we recolor the words from the dataset to the image's color
+# recolor just recolors the default colors to the image's blue color
+# interpolation is used to smooth the image generated 
+plt.imshow(wc.recolor(color_func=image_colors),interpolation="gaussian")
+
+plt.axis('off')
+plt.show()
+
+# function to extract hashtags from the dataset
+def Hashtags_Extract(x):
+    hashtags=[]
+    
+    # Loop over the words in the tweet
+    for i in x:
+        ht = re.findall(r'#(\w+)',i)
+        hashtags.append(ht)
+    
+    return hashtags
+
+ht_positive = Hashtags_Extract(combine['Tidy_Tweets'][combine['label']==0])
+#removing empty lists, and combining all the hashtags of different tweets into one array
+ht_positive_unnest = sum(ht_positive,[])
+#counting the number of times a particular hashtag occured.
+word_freq_positive = nltk.FreqDist(ht_positive_unnest)
+# creating a df for plotting the graph and better visibility of data
+df_positive = pd.DataFrame({'Hashtags':list(word_freq_positive.keys()),'Count':list(word_freq_positive.values())})
+#taking the 20 most frquent hashtags out
+df_positive_plot = df_positive.nlargest(20,columns='Count')
+
+sns.barplot(data=df_positive_plot,y='Hashtags',x='Count')
+sns.despine()
+
+ht_negative = Hashtags_Extract(combine['Tidy_Tweets'][combine['label']==1])
+ht_negative_unnest = sum(ht_negative,[])
+
+word_freq_negative = nltk.FreqDist(ht_negative_unnest)
+
+word_freq_negative
+
+df_negative = pd.DataFrame({'Hashtags':list(word_freq_negative.keys()),'Count':list(word_freq_negative.values())})
+
+df_negative_plot = df_negative.nlargest(20,columns='Count') 
+
+sns.barplot(data=df_negative_plot,y='Hashtags',x='Count')
+sns.despine()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+bow_vectorizer = CountVectorizer(max_df=0.90, min_df=2, max_features=1000, stop_words='english')
+
+# bag-of-words feature matrix
+bow = bow_vectorizer.fit_transform(combine['Tidy_Tweets'])
+
+df_bow = pd.DataFrame(bow.todense())
+
+df_bow
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf=TfidfVectorizer(max_df=0.90, min_df=2,max_features=1000,stop_words='english')
+tfidf_matrix=tfidf.fit_transform(combine['Tidy_Tweets'])
+df_tfidf = pd.DataFrame(tfidf_matrix.todense())
+df_tfidf
+
+train_bow = bow[:31962]
+train_bow.todense()
+train_tfidf_matrix = tfidf_matrix[:31962]
+train_tfidf_matrix.todense()
+
+from sklearn.model_selection import train_test_split
+# because we have to check our model's score in combination with feature extraction methods and models, we make separate training sets for bow and tf-idf
+x_train_bow, x_valid_bow, y_train_bow, y_valid_bow = train_test_split(train_bow,train['label'],test_size=0.3,random_state=2)
+x_train_tfidf, x_valid_tfidf, y_train_tfidf, y_valid_tfidf = train_test_split(train_tfidf_matrix,train['label'],test_size=0.3,random_state=17)
+
+from sklearn.metrics import f1_score
+from sklearn.linear_model import LogisticRegression
+Log_Reg = LogisticRegression(random_state=0,solver='lbfgs')
+# for bow
+Log_Reg.fit(x_train_bow,y_train_bow)
+
+prediction_bow = Log_Reg.predict_proba(x_valid_bow)
+
+prediction_bow
+
+# if prediction is greater than or equal to 0.3 than 1 else 0
+# Where 0 is for positive sentiment tweets and 1 for negative sentiment tweets
+prediction_int = prediction_bow[:,1]>=0.3
+
+# converting the results to integer type
+prediction_int = prediction_int.astype(np.int)
+prediction_int
+
+# calculating f1 score
+log_bow = f1_score(y_valid_bow, prediction_int)
+log_bow
+
+# for td-idf
+Log_Reg.fit(x_train_tfidf,y_train_tfidf)
+
+prediction_tfidf = Log_Reg.predict_proba(x_valid_tfidf)
+prediction_int = prediction_tfidf[:,1]>=0.3
+prediction_int = prediction_int.astype(np.int)
+prediction_int
+
+# calculating f1 score
+log_tfidf = f1_score(y_valid_tfidf, prediction_int)
+log_tfidf
+
+from sklearn.tree import DecisionTreeClassifier
+dct = DecisionTreeClassifier(criterion='entropy', random_state=1)
+
+dct.fit(x_train_bow,y_train_bow)
+dct_bow = dct.predict_proba(x_valid_bow)
+dct_bow
+
+# if prediction is greater than or equal to 0.3 than 1 else 0
+# Where 0 is for positive sentiment tweets and 1 for negative sentiment tweets
+dct_bow=dct_bow[:,1]>=0.3
+
+# converting the results to integer type
+dct_int_bow=dct_bow.astype(np.int)
+
+# calculating f1 score
+dct_score_bow=f1_score(y_valid_bow,dct_int_bow)
+
+dct_score_bow
+
+dct.fit(x_train_tfidf,y_train_tfidf)
+
+dct_tfidf = dct.predict_proba(x_valid_tfidf)
+
+dct_tfidf
+
+# if prediction is greater than or equal to 0.3 than 1 else 0
+# Where 0 is for positive sentiment tweets and 1 for negative sentiment tweets
+dct_tfidf=dct_tfidf[:,1]>=0.3
+
+# converting the results to integer type
+dct_int_tfidf=dct_tfidf.astype(np.int)
+
+# calculating f1 score
+dct_score_tfidf=f1_score(y_valid_tfidf,dct_int_tfidf)
+
+dct_score_tfidf
+
+Algo_1 = ['LogisticRegression(Bag-of-Words)','DecisionTree(Bag-of-Words)']
+
+score_1 = [log_bow,dct_score_bow]
+
+compare_1 = pd.DataFrame({'Model':Algo_1,'F1_Score':score_1},index=[i for i in range(1,3)])
+
+compare_1.T
+
+plt.figure(figsize=(18,5))
+
+sns.pointplot(x='Model',y='F1_Score',data=compare_1)
+
+plt.title('Bag-of-Words')
+plt.xlabel('MODEL')
+plt.ylabel('SCORE')
+
+plt.show()
+
+Algo_2 = ['LogisticRegression(TF-IDF)','DecisionTree(TF-IDF)']
+
+score_2 = [log_tfidf,dct_score_tfidf]
+
+compare_2 = pd.DataFrame({'Model':Algo_2,'F1_Score':score_2},index=[i for i in range(1,3)])
+
+compare_2.T
+
+plt.figure(figsize=(18,5))
+
+sns.pointplot(x='Model',y='F1_Score',data=compare_2)
+
+plt.title('TF-IDF')
+plt.xlabel('MODEL')
+plt.ylabel('SCORE')
+
+plt.show()
+
+Algo_best = ['LogisticRegression(Bag-of-Words)','LogisticRegression(TF-IDF)']
+
+score_best = [log_bow,log_tfidf]
+
+compare_best = pd.DataFrame({'Model':Algo_best,'F1_Score':score_best},index=[i for i in range(1,3)])
+
+compare_best.T
+
+plt.figure(figsize=(18,5))
+
+sns.pointplot(x='Model',y='F1_Score',data=compare_best)
+
+plt.title('Logistic Regression(Bag-of-Words & TF-IDF)')
+plt.xlabel('MODEL')
+plt.ylabel('SCORE')
+
+plt.show()
+
+test_tfidf = tfidf_matrix[31962:]
+test_pred = Log_Reg.predict_proba(test_tfidf)
+
+test_pred_int = test_pred[:,1] >= 0.3
+test_pred_int = test_pred_int.astype(np.int)
+
+test['label'] = test_pred_int
+
+submission = test[['id','label']]
+submission.to_csv('result.csv', index=False)
+
+res = pd.read_csv('result.csv')
+res
